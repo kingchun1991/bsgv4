408ea45a657b13922f9481f0d1f1f02ccd712ae2
King Chun O <kingchun1991@hotmail.com>
2022-12-23 21:01:52 +0800

    rootNotionPageId

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '8bab74ec2dd948b9b540b9fc0d174b8f',
+  rootNotionPageId: 'b132ee364cc346e088906a31b0ed285b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
